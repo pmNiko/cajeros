291c7c727e0607748beb0b510b66d0dbc2c3297c
erenyt12 <erenyt12@gmail.com>
2021-08-24 19:41:25 -0300

    ejercicio

---
+++ ejercicio.py
@@ -0,0 +1,24 @@
+# [17:18, 22/8/2021] Martin ( Programador ): Ejercicio de pago de sueldos de cajeros.
+# Se debe realizar un sistema que permita gestionar el pago de sueldos para los empleados encargados de atender la caja. Los empleados trabajan una determinada cantidad de horas mensuales( horas_trabajadas) la hora tiene un precio en base a la categoría: 
+# categoría_1 -> $250 
+# categoría_2 -> $380
+# categoría_3 -> $490
+# categoría_4 -> $600
+
+# Al finalizar el mes se debe calcular el sueldo correspondiente a cada empleado. 
+
+# El sistema debe tener el siguiente menú:
+
+# 1- opcion dar de alta un cajero, 
+# 2- cambiar categoria de un cajero, 
+# 3- eliminar cajero,
+# 4- calcular pago
+# 5- subir porcentaje de valor de categorias
+# 6- salir del sistema"""
+# [17:24, 22/8/2021] Martin ( Programador ): Premisas: 
+# --- para dar de alta un cajero se debe pedir el nombre y asociarlo por defecto a la categoría_1
+# --- cambiar la categoría de un cajero, se debe asociar a la nueva categoría especificada disponible.
+# --- cálculo de pago: muestra una tabla con los pagos a realizar a cada cajero.
+# --- subir porcentaje de pago, se debe ingresar un porcentaje de aumento para afectar el valor de todas las categorías.
+
+# Implementar funciones en un archivo a parte para luego importarlas y hacer uso en cada opción según corresponda.
